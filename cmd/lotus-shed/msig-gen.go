a1689b6275ddc210dede67860e677fb7edbea064
whyrusleeping <why@ipfs.io>
2020-09-16 23:50:13 -0700

    add 'next' method to walk the process forward

--- cmd/lotus-shed/msig-gen.go
+++ cmd/lotus-shed/msig-gen.go
@@ -14,9 +14,11 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
 
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	iact "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	msig "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
@@ -52,6 +54,7 @@ var createMsigsCmd = &cli.Command{
 	Subcommands: []*cli.Command{
 		msigCreateStartCmd,
 		msigCreationStatusCmd,
+		msigCreateNextCmd,
 	},
 }
 
@@ -161,7 +164,7 @@ var msigCreateStartCmd = &cli.Command{
 				return xerrors.Errorf("failed to create multisigs: %w", err)
 			}
 
-			cd.Jobs = append(cd.Jobs, MsigCreationProgress{
+			cd.Jobs = append(cd.Jobs, &MsigCreationProgress{
 				Params:    p,
 				CreateCID: createCid,
 			})
@@ -276,36 +279,82 @@ var msigCreateNextCmd = &cli.Command{
 			return err
 		}
 
+		fi.Close()
+
+		writeProgress := func() error {
+			nfi, err := os.Create(cctx.Args().First())
+			if err != nil {
+				return fmt.Errorf("failed to open file: %w", err)
+			}
+			defer nfi.Close()
+
+			if err := json.NewEncoder(nfi).Encode(&msd); err != nil {
+				return fmt.Errorf("failed to write progress out to a file: %w", err)
+			}
+
+			return nil
+		}
+
 		for _, job := range msd.Jobs {
 			if job.ActorID == address.Undef {
 				r, err := api.StateGetReceipt(ctx, job.CreateCID, types.EmptyTSK)
 				if err != nil {
 					log.Warnf("no receipt found for %s", job.CreateCID)
 				} else {
-					if r.ExitCode != 0 {
-						fmt.Printf("creation job failed for %s %s %s: %d\n", job.Params.Name, job.Params.Custodian, job.Params.Entity, r.ExitCode)
-					} else {
-						var er iact.ExecReturn
-						if err := er.UnmarshalCBOR(bytes.NewReader(r.Return)); err != nil {
-							return xerrors.Errorf("return value of create message failed to parse: %w", err)
-						}
+					if r != nil {
+						if r.ExitCode != 0 {
+							fmt.Printf("creation job failed for %s %s %s: %d\n", job.Params.Name, job.Params.Custodian, job.Params.Entity, r.ExitCode)
+						} else {
+							var er iact.ExecReturn
+							if err := er.UnmarshalCBOR(bytes.NewReader(r.Return)); err != nil {
+								return xerrors.Errorf("return value of create message failed to parse: %w", err)
+							}
 
-						fmt.Printf("actor create successful: %s %s %s %s\n", job.Params.Name, job.Params.Custodian, job.Params.Entity, er.RobustAddress)
-						job.ActorID = er.RobustAddress
+							fmt.Printf("actor create successful: %s %s %s %s\n", job.Params.Name, job.Params.Custodian, job.Params.Entity, er.RobustAddress)
+							job.ActorID = er.RobustAddress
+							if err := writeProgress(); err != nil {
+								return err
+							}
+						}
+					} else {
+						fmt.Printf("creation message for %s hasnt made it into the chain yet\n", job.Params.Custodian)
 					}
 				}
 			}
 
 			if job.ActorID != address.Undef && !job.SetVestingCID.Defined() {
+				famt, err := types.ParseFIL(job.Params.Amount)
+				if err != nil {
+					return xerrors.Errorf("failed to parse fil amount: %w", err)
+				}
+				const blocksInADay = (24 * 60 * 60) / 30
+				const blocksInAMonth = (blocksInADay * 365) / 12
 				params := msig.LockBalanceParams{
 					StartEpoch:     0,
-					UnlockDuration: 1,
-					Amount:         job.Params.Amount,
+					UnlockDuration: abi.ChainEpoch(blocksInAMonth * job.Params.VestingMonths),
+					Amount:         big.Int(famt),
 				}
+
+				buf := new(bytes.Buffer)
+				if err := params.MarshalCBOR(buf); err != nil {
+					return err
+				}
+
 				msg := &types.Message{
 					From:   msd.Creator,
 					To:     job.ActorID,
 					Method: builtin.MethodsMultisig.LockBalance,
+					Params: buf.Bytes(),
+				}
+
+				sm, err := api.MpoolPushMessage(ctx, msg, nil)
+				if err != nil {
+					return err
+				}
+
+				job.SetVestingCID = sm.Cid()
+				if err := writeProgress(); err != nil {
+					return err
 				}
 			}
 		}
